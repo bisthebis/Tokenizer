3eaf20f8562f74c77eaca8f35516c9acc0b94e2b
Boris Martin <bism25@hotmail.com>
2016-10-18 21:18:12 +0200

    C++ 14 and fix

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
 
-	QTimer::singleShot(1000, [](){qDebug() << "Leaving..."; qApp->quit();}
+	QTimer::singleShot(1000, [](){qDebug() << "Leaving..."; qApp->quit();});
 	
 	return app.exec();
 
