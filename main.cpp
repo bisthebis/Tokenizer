ffb9fe989a4ff8a2ca710a7eabd0f55601109660
Boris Martin <bism25@hotmail.com>
2016-10-18 21:15:17 +0200

    Initial

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QtCore>
+
+int main(int argc, char** argv)
+{
+	QCoreApplication app(argc, argv);
+
+	QTimer::singleShot(1000, [](){qDebug() << "Leaving..."; qApp->quit();}
+	
+	return app.exec();
+
+}
